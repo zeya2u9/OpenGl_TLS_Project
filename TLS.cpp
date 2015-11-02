90db859526deadb6278e2b44ac128a05c5ef712d
Zeya Umayya <zeya2u9@users.noreply.github.com>
2015-11-02 14:33:18 +0530

    Update TLS.cpp

--- TLS.cpp
+++ TLS.cpp
@@ -10,20 +10,30 @@ int view=0;
 void text(int x,int y,char *string,int font)
 {
 	int i=0;
-	glColor3f(0.5,0.5,0.8);
+	glColor3f(0.0,0.5,0.8);
 	glRasterPos2f(x,y);
 	for(i=0;i<strlen(string);i++)
 	{
 		if(font==1)
 			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,string[i]);
 		else if(font==2)
-			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_10,string[i]);
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,string[i]);
+		else if(font==3)
+		{
+			glColor3f(1.0,0.0,0.0);
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,string[i]);
+		}
 	}
 }
 void First_win()
 {
 	glClearColor(1.0,1.0,0.6,1.0);
 	text(290,700,"TRAFFIC LIGHT SIMULATOR",1);
+	text(350,660,"Using OpenGL",1);
+	text(400,620,"Made By:",2);
+	text(300,580,"Zeya Umayya & Zishan Ahmad",1);
+	text(100,540,"INSTRUCTIONS::",2);
+	text(300,100,"!!!!PRESS SPACEBAR TO ENTER INTO THE ERENA!!!!",3);
 	glutPostRedisplay();
 	//glutSwapBuffers();
 }
@@ -46,8 +56,7 @@ void main_display()
 		init();
 		First_win();
 	}
-	//glutWireSphere(1.0,15,20);
-	//arrow();
+	
 	glutSwapBuffers();
 }
 void keyboard(unsigned char key,int x,int y)
