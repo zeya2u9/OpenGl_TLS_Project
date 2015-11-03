dc9b3e19833cc39c1649affda74ad3b251ceccfb
Zeya Umayya <zeya2u9@users.noreply.github.com>
2015-11-03 11:10:06 +0530

    Update TLS.cpp

--- TLS.cpp
+++ TLS.cpp
@@ -1,4 +1,4 @@
-// TLS.cpp : Defines the entry point for the console application.
+// traffic.cpp : Defines the entry point for the console application.
 //
 #include "stdafx.h"
 #include<GL/glut.h>
@@ -33,7 +33,7 @@ void First_win()
 	text(400,620,"Made By:",2);
 	text(300,580,"Zeya Umayya & Zishan Ahmad",1);
 	text(100,540,"INSTRUCTIONS::",2);
-	text(300,100,"!!!!PRESS SPACEBAR TO ENTER INTO THE ERENA!!!!",3);
+	text(300,100,"!!!!PRESS SPACEBAR TO ENTER INTO THE ARENA!!!!",3);
 	glutPostRedisplay();
 	//glutSwapBuffers();
 }
@@ -46,6 +46,76 @@ void init()
 	glOrtho(0.0,1000,0.0,800,50.0,-50.0);
 	glutPostRedisplay();
 }
+void road_strips()
+{
+	//first bottom green strip
+	glPushMatrix();
+	glBegin(GL_POLYGON);
+	glColor3f(0.0,0.8,0.0);
+		glVertex2d(0,200);
+		glVertex2d(0,250);
+		glVertex2d(1000,250);
+		glVertex2d(1000,200);
+	glEnd();
+	glPopMatrix();
+
+	//second grey strip
+	glPushMatrix();
+	glBegin(GL_POLYGON);
+	glColor3f(0.5,0.5,0.5);
+		glVertex2d(0,250);
+		glVertex2d(0,500);
+		glVertex2d(1000,500);
+		glVertex2d(1000,250);
+	glEnd();
+	glPopMatrix();
+
+	//tilted grey strip
+	glPushMatrix();
+	glBegin(GL_POLYGON);
+	glColor3f(0.5,0.5,0.5);
+		glVertex2d(300,500);
+		glVertex2d(200,800);
+		glVertex2d(350,800);
+		glVertex2d(500,500);
+	glEnd();
+	glPopMatrix();
+
+	//Dotted strips
+
+
+	//traffic signal controller
+	glPushMatrix();
+	glBegin(GL_POLYGON);
+	glColor3f(0.7,0.3,0.0);
+		glVertex2d(600,350);  //1
+		glVertex2d(650,350);  //2
+		glVertex2d(650,370);  //3..
+		glVertex2d(600,370);//12
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
+	glBegin(GL_POLYGON);
+	glColor3f(0.6,0.2,0.0);
+		glVertex2d(630,370);  //4
+		glVertex2d(630,500);  //5
+		glVertex2d(620,500);//10
+		glVertex2d(620,370);//11
+	glEnd();
+	glPopMatrix();
+
+	glPushMatrix();
+	glBegin(GL_POLYGON);
+	glColor3f(0.7,0.3,0.0);
+		glVertex2d(640,500);  //6
+		glVertex2d(640,700);  //7
+		glVertex2d(610,700);  //8
+		glVertex2d(610,500); //9		
+	glEnd();
+	glPopMatrix();
+
+}
 void main_display()
 {
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
@@ -56,13 +126,19 @@ void main_display()
 		init();
 		First_win();
 	}
-	
+	else
+	{
+		//---------CONSTANT PARTS
+		//ROAD STRIP
+		road_strips();
+	}
 	glutSwapBuffers();
 }
 void traffic_start()
 {}
 void keyboard(unsigned char key,int x,int y)
 {
+	
 	switch(key)
 	{
 		case ESCAPE:exit(1);
